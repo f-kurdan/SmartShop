e92d4d99023fc2258edbaec641b56f20a27957ff
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-12 18:35:50 +0300

    Create data models

---
+++ online-store/server/src/models/order-line.model.ts
@@ -0,0 +1,8 @@
+export default class OrderLine {
+    id:number;
+    productItemId: number;
+    orderId: number;
+    quantity: string;
+    price: number;
+}   
+
---
+++ online-store/server/src/models/order-status.model.ts
@@ -0,0 +1,4 @@
+export default class OrderStatus {
+    id: number;
+    status: string; 
+}
