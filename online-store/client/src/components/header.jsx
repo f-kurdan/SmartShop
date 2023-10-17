503a0211d08a0630c90752cfd4f12cbb9c7273d6
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-17 19:57:45 +0300

    Update header component

--- online-store/client/src/components/header.jsx
+++ online-store/client/src/components/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="fixed p-2 flex flex-row align-middle border-solid w-full h-auto bg-gray-800">
             <h3 className='text-gray-50 px-1'>SmartShop</h3>
-            <div className=' flex flex-row justify-self-end'>
+            <div className='absolute right-3 flex flex-row justify-items-end'>
                 <h3 className='text-gray-50 px-1'>Админ</h3>
                 <h3 className='text-gray-50 px-1'>Войти</h3>
                 <h3 className='text-gray-50 px-1'>Выйти</h3>
