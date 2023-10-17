f23da0ebb42ea3b0817bf7df67fa0ac1742d3b5b
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-17 21:35:11 +0300

    Update main page

--- online-store/client/src/app/page.tsx
+++ online-store/client/src/app/page.tsx
@@ -1,9 +1,17 @@
+import Categories from '@/components/categories'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='bg-slate-50'>
-      main content
-    </main>
+    <>
+    
+    <Categories />
+    </>
   )
 }
+
+    // <div className='flex  w-full h-auto absolute top-10 bg-slate-50'>
+    //   <div className="w-1/5 grow-0 border-2 border-solid border-black">first col</div>
+    //   <div className="min-w-max grow border-2 border-solid border-black">second col</div>
+    //   <div className="w-1/5 grow-0 border-2 border-solid border-black">third col</div>
+    // </div>
