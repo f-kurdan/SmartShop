3d77a8c71090cbfec19d4c7211b28ba353537df7
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-20 10:00:51 +0300

    rename file

--- online-store/client/src/components/main-page/categories.tsx
+++ online-store/client/src/components/main-page/categories.tsx
@@ -15,7 +15,7 @@ const Categories = () => {
       {categories.map((cat, index) => (
         <div key={index} className='w-60 flex flex-col justify-center items-center p-7 bg-white  min-w-fit rounded-3xl shadow-lg transition duration-300 hover:scale-105 hover:cursor-pointer ' >
           <img className='w-32 h-32'  src={cat.src} alt="category" />
-          <div className='mt-3 text-base text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-600' >{cat.name}</div>
+          <div className='mt-3 text-base text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-green-500 to-blue-500' >{cat.name}</div>
         </div>
       ))}
     </div>
--- online-store/client/src/components/navbar.jsx
+++ online-store/client/src/components/navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const textStyle = 'text-black text-sm';
 const iconStyle = 'w-5 h-5';
-const linkStyle = 'hover:bg-gradient-to-r hover:from-yellow-300 hover:to-orange-300 flex flex-col justify-center items-center mx-1 rounded-md p-1 hover:cursor-pointer ';
+const linkStyle = 'hover:invert hover:bg-gradient-to-br hover:from-yellow-300 hover:via-red-500 hover:to-yellow-300 flex flex-col justify-center items-center mx-1 rounded-md p-1 hover:cursor-pointer ';
 
 const Navbar = () => {
     return (
         <div className="fixed top-0 flex flex-row align-middle items-center border-solid w-full h-14 bg-white z-10 shadow-sm">
-            <h3 className='mx-3 bg-gradient-to-r from-indigo-500 via-sky-600 to-blue-700 hover:from-purple-950 hover:via-red-700 hover:to-yellow-600 rounded-2xl p-2 text-white hover:cursor-pointer'>SmartShop</h3>
+            <h3 className='mx-3 bg-gradient-to-br from-indigo-500 via-sky-600 to-blue-700 hover:from-purple-950 hover:via-red-700 hover:to-yellow-600 rounded-2xl p-2 text-white hover:cursor-pointer'>SmartShop</h3>
             <div className='absolute right-2 flex flex-row justify-items-end'>
                 <div className={linkStyle}>
                     <img className={iconStyle} src="icons/admin.svg" alt="" />
