ebc424f64e7101a72b0f28ed94fe8d951cac5e5b
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-25 14:20:31 +0300

    Add images

--- online-store/new-client/src/pages/index.tsx
+++ online-store/new-client/src/pages/index.tsx
@@ -2,8 +2,6 @@ import Banner from '@/components/home-page/banner'
 import Categories from '@/components/home-page/categories'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <div className='flex flex-col justify-evenly items-center h-5/6 min-h-screen'>
