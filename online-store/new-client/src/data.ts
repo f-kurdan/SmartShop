ad5d19962bcaaca09872a4f45b22b8db7952c7db
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-25 14:09:56 +0300

    Add data file

---
+++ online-store/new-client/src/data.ts
@@ -0,0 +1,83 @@
+export const categories = [
+    { slug: "phones", name: "ТЕЛЕФОНЫ", src: "phone.png" },
+    { slug: "headphones", name: "НАУШНИКИ", src: "headphone.png" },
+    { slug: "tablet", name: "ПЛАНШЕТЫ", src: "tablet.png" },
+    { slug: "watch", name: "СМАРТ-ЧАСЫ", src: "watch.png" },
+    { slug: "laptop", name: "НОУТБУКИ", src: "laptop.png" },
+    { slug: "camera", name: "КАМЕРЫ", src: "camera.png" },
+]
+
+export const phones = [
+    {
+        "name": "Apple iPhone 15 Pro Max eSIM 256 ГБ, «титановый бежевый»",
+        "brand": "Apple",
+        "display": '6,1" (2556x1179), OLED',
+        "chip": 'A17 Pro',
+        "capacity": '128GB',
+        "price": "204 990 ₽",
+        "camera": '48/12/12',
+        "photo": "/phones/iphone15.jpeg",
+    },
+    {
+        "name": "Apple iPhone 15 Pro Max eSIM 256 ГБ, «титановый бежевый»",
+        "brand": "Apple",
+        "display": '6,1" (2556x1179), OLED',
+        "chip": 'A17 Pro',
+        "capacity": '128GB',
+        "price": "204 990 ₽",
+        "camera": '48/12/12',
+        "photo": "/phones/iphone15.jpeg",
+    },
+    {
+        "name": "Apple iPhone 15 Pro Max eSIM 256 ГБ, «титановый бежевый»",
+        "brand": "Apple",
+        "display": '6,1" (2556x1179), OLED',
+        "chip": 'A17 Pro',
+        "capacity": '128GB',
+        "price": "204 990 ₽",
+        "camera": '48/12/12',
+        "photo": "/phones/iphone15.jpeg",
+    },
+    {
+        "name": "Apple iPhone 15 Pro Max eSIM 256 ГБ, «титановый бежевый»",
+        "brand": "Apple",
+        "display": '6,1" (2556x1179), OLED',
+        "chip": 'A17 Pro',
+        "capacity": '128GB',
+        "price": "204 990 ₽",
+        "camera": '48/12/12',
+        "photo": "/phones/iphone15.jpeg",
+    }
+]
+
+export const headphones = [
+    {
+        "name": "Наушники Apple AirPods 2, Bluetooth, вкладыши, белый ",
+        "frequency_range": 'от 20 Гц до 15 кГц',
+        "connection_type": 'беспроводные bluetooth, True Wireless',
+        "mic": 'встроенный',
+        "battery_life": '5 ч',
+        "price": "12 990 ₽",
+        "photo": "/headphones/airpods.webp"
+    },
+    {
+        "name": "Наушники Apple AirPods 2, Bluetooth, вкладыши, белый ",
+        "frequency_range": 'от 20 Гц до 15 кГц',
+        "connection_type": 'беспроводные bluetooth, True Wireless',
+        "mic": 'встроенный',
+        "battery_life": '5 ч',
+        "price": "12 990 ₽",
+        "photo": "/headphones/airpods.webp"
+
+    },
+    {
+        "name": "Наушники Apple AirPods 2, Bluetooth, вкладыши, белый ",
+        "frequency_range": 'от 20 Гц до 15 кГц',
+        "connection_type": 'беспроводные bluetooth, True Wireless',
+        "mic": 'встроенный',
+        "battery_life": '5 ч',
+        "price": "12 990 ₽",
+        "photo": "/headphones/airpods.webp"
+
+    },
+]
