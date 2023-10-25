a41d1bfc73dc2fa6613058f4288fe7806865e982
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-25 14:26:26 +0300

    Add google fonts

--- online-store/new-client/src/components/layout.tsx
+++ online-store/new-client/src/components/layout.tsx
@@ -1,6 +1,12 @@
+import { Montserrat, Rubik, Syne, Unbounded } from 'next/font/google';
 import React from 'react'
 import Footer from './footer'
 import Navbar from './navbar'
+
+const unbounded = Unbounded({ subsets: ["cyrillic", "latin"], weight: "300", display: "swap", variable: '--var-unbounded' });
+const montserrat = Montserrat({ subsets: ["cyrillic", "latin"], weight: "500", display: "swap", variable: '--var-mont' });
+const rubik = Rubik({ subsets: ["cyrillic", "latin"], display: "swap", variable: '--var-rubik' });
+const syne = Syne({ subsets: ["latin"], weight: "800", display: "swap", variable: '--var-syne' });
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${unbounded.variable} ${montserrat.variable} ${rubik.variable} ${syne.variable}`}>
       <body className='bg-gradient-to-br from-white via-black to-purple-700'>
         <main className='min-h-screen'>
           <Navbar />
