c6833c612d6cbff142a1c6b459b36401af92783c
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-26 12:27:39 +0300

    Add dynamic route for categories

---
+++ online-store/new-client/src/pages/catalog/[category].tsx
@@ -0,0 +1,23 @@
+import PhonesList from '@/components/item-lists/phone-list'
+import React, { ReactNode, useState } from 'react'
+
+
+const getCategory = (category: string, brands: string[] ) => {
+    switch (category) {
+        case "phones": 
+        return (<PhonesList filteredBrands={brands}/>);
+    }
+}
+const Category = ({category}:{category: string}) => {
+    const [filteredBrands, setFilteredBrands] = useState<string[]>([]);
+
+    return (
+        <div className='flex flex-row justify-around items-start mt-3'>
+            <div className='flex flex-col w-2/3 min-h-fit mr-10'>
+                { }
+            </div>
+        </div>
+    )
+}
+
+export default Category
