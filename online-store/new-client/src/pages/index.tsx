9b46c7d463b80788d271d90c37361a99345ae4c9
Kurdanov Feytullah <fkurdan@gmail.com>
2023-10-28 10:44:26 +0300

    Add react query to the project

--- online-store/new-client/src/pages/index.tsx
+++ online-store/new-client/src/pages/index.tsx
@@ -4,26 +4,30 @@ import Navbar from '@/components/navbar';
 import { categories } from '@/data';
 import { category } from '@/types';
 import { Inter } from 'next/font/google'
+import { QueryClient } from 'react-query';
+import { QueryClientProvider } from 'react-query/types/react';
 
 export async function getStaticProps() {
   const cats = categories;
   return {
-      props: {
-          cats,
-      },
+    props: {
+      cats,
+    },
 
-      revalidate: 60,
+    revalidate: 60,
   }
 }
 
-export default function Home({cats}:{cats:category[]}) {
+const queryClient = new QueryClient()
+
+export default function Home({ cats }: { cats: category[] }) {
   return (
-    <>
-    <Navbar categories={cats} />
-    <div className='flex flex-col gap-5 justify-around items-center h-5/6 min-h-screen'>
-      <Banner />
-      <Categories />
-    </div>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Navbar categories={cats} />
+      <div className='flex flex-col gap-5 justify-around items-center h-5/6 min-h-screen'>
+        <Banner />
+        <Categories />
+      </div>
+    </QueryClientProvider>
   )
 }
