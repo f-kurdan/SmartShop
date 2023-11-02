df74818c33b940a05b450025fee3d61d326fbed0
Kurdanov Feytullah <fkurdan@gmail.com>
2023-11-02 16:59:14 +0300

    Update no items components

--- online-store/new-client/src/components/item-lists/no-items.tsx
+++ online-store/new-client/src/components/item-lists/no-items.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const NoItems = () => {
     return (
-        <div className=' bg-white rounded-xl shadow-lg shadow-black/30 px-10 py-7 '>
-            Нет товаров ¯\_(ツ)_/¯
+        <div className='flex flex-col w-2/3 min-h-fit mr-10'>
+            <div className=' bg-white rounded-xl shadow-lg shadow-black/30 px-10 py-7 '>
+                Нет товаров ¯\_(ツ)_/¯
+            </div>
         </div>
     )
 }
