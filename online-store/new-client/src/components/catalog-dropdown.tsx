8e1f1e07c38256df452e0b0488a35d0a0a54c36a
Kurdanov Feytullah <fkurdan@gmail.com>
2023-11-07 11:46:16 +0300

    Change categories dropdown list's style

--- online-store/new-client/src/components/catalog-dropdown.tsx
+++ online-store/new-client/src/components/catalog-dropdown.tsx
@@ -8,10 +8,10 @@ const CatalogDropdown = () => {
     return (
         <div className="absolute opacity-100 left-5 group shadow-md p-1 inline-block w-40 text-center">
             <button className="group-hover:opacity-0">Категории</button>
-            <div className="  border-black border-2 gap-1 border-solid rounded-lg left-0 top-8 absolute w-40 bg-slate-50 hidden z-10  group-hover:block">
+            <div className="shadow-md left-0 top-8 absolute w-40 bg-slate-50 hidden z-10  group-hover:block">
                 {isCategoriesLoading ? (<span>Идет загрузка...</span>)
                 : categories?.length ? categories?.map((cat) =>
-                    (<Link key={cat.id} className='hover:bg-blue-100 rounded-lg block' href={`/catalog/${cat.id}`}>{cat.name}</Link>)
+                    (<Link key={cat.id} className='hover:bg-lime-200 block' href={`/catalog/${cat.id}`}>{cat.name}</Link>)
                 ) : (
                     <span>Нет данных...</span>
                 )}
