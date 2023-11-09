c2d36508e76db4a3f39c15b523ccbd5e22eb8533
Kurdanov Feytullah <fkurdan@gmail.com>
2023-11-09 18:10:39 +0300

    Update login page

--- online-store/new-client/src/pages/auth/index.tsx
+++ online-store/new-client/src/pages/auth/index.tsx
@@ -3,18 +3,27 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className={`${montserrat.className} flex flex-col gap-12  justify-center items-center px-10 py-5 my-3 w-11/12 ounded-sm border border-gray-200 bg-white shadow-lg rounded-2xl`}>
+    <div className={`${montserrat.className} flex flex-col gap-10  justify-center items-center px-10 py-5 my-3 w-11/12 ounded-sm border border-gray-200 bg-white shadow-lg rounded-2xl`}>
       <div className='font-bold text-5xl text-center text-gray-600 '>
         Вход
       </div>
-        <div className='flex flex-col w-2/5 gap-2'>
-            <h1 className='text-gray-600'>Email*</h1>
-              <input type="email" className='bg-slate-100 rounded-lg p-4  focus:bg-slate-200 focus:outline-none' placeholder='qwerty@pochta.ru'/>
+      <div className='flex flex-col w-2/5 gap-2'>
+        <h1 className=''>Email*</h1>
+        <input type="email" className='bg-slate-100 rounded-lg p-4  focus:bg-slate-200 focus:outline-none' placeholder='qwerty@pochta.ru' />
+      </div>
+      <div className='flex flex-col w-2/5 gap-2'>
+        <h1 className=''>Пароль*</h1>
+        <input type="password" className='bg-slate-100 rounded-lg p-4 focus:bg-slate-200 focus:outline-none' placeholder='Введите пароль' />
+        <div className='grid grid-cols-2 gap-2 text-violet-800 font-medium'>
+          <div>
+            <input type="checkbox" id='save' className='hover:cursor-pointer mr-3 scale-125' />
+            <label htmlFor="save">Запомнить меня</label>
+          </div>
+          <div className='text-end'>
+            <a href="." >Забыли пароль?</a>
+          </div>
         </div>
-        <div className='flex flex-col w-2/5 gap-2'>
-            <h1 className='text-gray-600'>Пароль*</h1>
-              <input type="password" className='bg-slate-100 rounded-lg p-4 focus:bg-slate-200 focus:outline-none' placeholder='Введите пароль'/>
-        </div>        
+      </div>
     </div>
   )
 }
