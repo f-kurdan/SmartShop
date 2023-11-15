0158a9228d74e48ee004e6bfbdb34af86653cdeb
Kurdanov Feytullah <fkurdan@gmail.com>
2023-11-15 08:33:25 +0300

    Add debponcing to search

--- online-store/new-client/src/components/navbar/search.tsx
+++ online-store/new-client/src/components/navbar/search.tsx
@@ -4,26 +4,27 @@ import { productsList } from '@/types'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import React, { useRef, useState } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
 
 const Search = () => {
     const searchParams = useSearchParams()
     const { replace } = useRouter()
     const inputRef = useRef<HTMLInputElement>(null)
     const params = new URLSearchParams(searchParams)
-    let { data } = useProducts()
+    const { data } = useProducts()
     const [searchOptions, setSearchOptions] = useState<productsList | string[]>()
 
     const placeholder = 'Введите название товара'
     const catalog = '/catalog'
 
-    const handleChange = (value: string) => {
+    const handleChange = useDebouncedCallback((value: string) => {
         if (value) {
             const products = data?.filter(product => product.name.toLowerCase().includes(value.toLowerCase()))
                 .slice(0, 5)
             setSearchOptions(!!products?.length ? products : [`По запросу ${value} ичего не найдено`])
         }
         else setSearchOptions([])
-    }
+    }, 300)
 
     const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
@@ -73,11 +74,11 @@ const Search = () => {
                 defaultValue={searchParams.get('query')?.toString()}
             />
             <MagnifyingGlassIcon onClick={onIconClick} className="absolute hover:cursor-pointer left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            <div className='bg-white w-fit  absolute top-full left-0py-2 px-3 border border-gray-200 rounded-md shadow-md hidden group-focus-within:block hover:block' >
+            <div className='bg-white w-fit absolute top-full left-0py-2 px-3 rounded-md shadow-md hidden group-focus-within:block hover:block'>
                 {!!searchOptions?.length && searchOptions.map(option =>
                     (typeof option === "string") ? (<div className='p-3'>{option}</div>) :
                         (
-                            <div className='hover:cursor-pointer' onClick={() => onOptionClick(option?.name.toLowerCase())}>{option?.name}</div>
+                            <div className='hover:cursor-pointer text-sm p-2 my-2 hover:bg-gradient-to-br hover:text-white hover:from-indigo-500 hover:via-sky-600 hover:to-blue-700 block active:from-purple-950 active:via-red-700 active:to-yellow-600 rounded-md ' onClick={() => onOptionClick(option?.name.toLowerCase())}>{option?.name}</div>
                         ))}
             </div>
         </div>
