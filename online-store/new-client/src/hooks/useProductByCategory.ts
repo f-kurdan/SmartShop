4052a08edcbe65c43bc0d38665c3c6b4f5fae48a
Kurdanov Feytullah <fkurdan@gmail.com>
2023-11-24 15:48:56 +0300

    Delete unused hooks

--- online-store/new-client/src/hooks/useProductByCategory.ts
+++
@@ -1,8 +0,0 @@
-import { getProductByCategory } from '@/services/product.service'
-import { useQuery } from 'react-query'
-
-const useProductByCategory = (categoryId:string) => {
-  return useQuery(["products", categoryId], () => getProductByCategory(categoryId), { enabled: !!categoryId})
-}
-
-export default useProductByCategory
--- online-store/new-client/src/hooks/useTotalProducts.ts
+++
@@ -1,8 +0,0 @@
-import { getTotalProducts } from "@/services/product.service"
-import { useQuery } from "react-query"
-
-const useTotalProducts = () => {
-    return useQuery(["products"], getTotalProducts)
-}
-
-export default useTotalProducts
