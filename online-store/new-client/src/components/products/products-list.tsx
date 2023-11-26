d32f1d108c838b5f122e214b9866a7c9b90b850e
Kurdanov Feytullah <fkurdan@gmail.com>
2023-11-26 12:49:58 +0300

    Update products lisy layout

--- online-store/new-client/src/components/products/products-list.tsx
+++ online-store/new-client/src/components/products/products-list.tsx
@@ -7,11 +7,14 @@ const ProductsList = ({ products }: { products: productsList }) => {
     return (
         <div className='flex flex-row gap-2 flex-wrap justify-start items-center'>
             {products.map((product) =>
-            (<Link href={`/products/${product.id}`}>
-                <div key={product.id} className='transition-all duration-300 flex flex-col gap-2 w-72 justify-center items-center p-5 bg-white hover:shadow-lg hover:shadow-black/30 hover:cursor-pointer active:opacity-80'>
-                    <Image className='max-w-48 max-h-48' src={product.photo} alt={product.name} width={160} height={160} />
-                    <p className='text-sm text-center  hover:text-blue-600 active:text-lime-400 '>{product.name}</p>
-                    <p className='text-md text-center font-bold'>{product.price}</p>
+            (<Link className='' href={`/products/${product.id}`}>
+                <div key={product.id} className=' transition-all duration-300 flex flex-col gap-2 justify-center items-center p-5 bg-white hover:shadow-lg hover:shadow-black/30 hover:cursor-pointer active:opacity-80 w-72 h-80'>
+                    <Image className='max-h-44 max-w-min' src={product.photo} alt={product.name} width={200} height={200} />
+                    <div className='justify-self-end'>
+                        <div className='text-sm text-center  hover:text-purple-600 active:text-lime-400 '>
+                            {product.name}</div>
+                        <div className='text-md text-center font-bold'>{product.price}</div>
+                    </div>
                 </div>
             </Link>)
             )}
