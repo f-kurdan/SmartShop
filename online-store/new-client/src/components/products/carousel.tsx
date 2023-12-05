1b9ef2b1c5165cdedfc60ce90ffb51ab502887d9
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-05 12:52:03 +0300

    Update carousel

--- online-store/new-client/src/components/products/carousel.tsx
+++ online-store/new-client/src/components/products/carousel.tsx
@@ -6,23 +6,36 @@ import React, { useState } from 'react'
 
 
 const urls = [
-    'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D%3D%3D',
-    'https://images.unsplash.com/photo-1612442058361-17…xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1508685096489-7a…xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1579065560489-98…xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    '/phones/samsung_galaxy_s23_black/main.png',
+    '/phones/samsung_galaxy_s23_black/front.avif',
+    '/phones/samsung_galaxy_s23_black/left-side.avif',
+    '/phones/samsung_galaxy_s23_black/cameras.avif'
 ]
 
 const Carousel = () => {
-
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    const onLeftClick = () => {
+        const isFirstImage = currentIndex === 0;
+        const newIndex = isFirstImage ? urls.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    }
+
+    const onRightClick = () => {
+        const isLastImage = currentIndex === urls.length - 1;
+        console.log('currentIndex: ' + currentIndex)
+        console.log(currentIndex + 1)
+        const newIndex = isLastImage ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    }
 
     return (
         <div className='transition-all duration-300 group relative'>
-            <div className='transition-all duration-300 group-hover:block hidden cursor-pointer p-4 absolute left-1 top-1/2 rounded-full bg-white opacity-70' >
+            <div onClick={onLeftClick} className='transition-all duration-300 group-hover:block hidden cursor-pointer p-4 absolute left-1 top-1/2 rounded-full bg-white opacity-70' >
                 <ArrowLeftIcon width={20} height={20} />
             </div>
             <Image className='max-h-96 max-w-min object-cover transition duration-500' src={urls[currentIndex]} alt={'data!.name'} width={400} height={400} />
-            <div className='group-hover:block hidden cursor-pointer p-4 absolute right-1 top-1/2 rounded-full bg-white opacity-70' >
+            <div onClick={onRightClick} className='group-hover:block hidden cursor-pointer p-4 absolute right-1 top-1/2 rounded-full bg-white opacity-70' >
                 <ArrowRightIcon width={20} height={20} />
             </div>
         </div>
