b40b2a83c6993fdeea3bfb5483722c04d166b478
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-09 18:19:39 +0300

    Implementadding and removing of  specs

--- online-store/new-client/src/components/admin/product-specification-adding.tsx
+++ online-store/new-client/src/components/admin/product-specification-adding.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import ProductSpecificationRow from './product-specification-row'
 
+type ProductSpec = { title: string, description: string, id: number }
+
 const ProductSpecificationAdding = () => {
+    const [rows, setRows] = React.useState<ProductSpec[]>([])
+
+    console.log(rows)
+
+    const handleAddRow = () => {
+        setRows(rows => [...rows, { "title": "", "description": "", id: Date.now() }])
+    }
+
+    const handleDeleteRow = (id: number) => {
+        console.log(id)
+    setRows(rows.filter(r => r.id!== id))
+    }
+
     return (
         <div className='flex flex-col gap-3 w-11/12 justify-center items-start'>
-            <div className='p-3 rounded-lg bg-blue-200 cursor-pointer border-2 border-black text-center'>
+            <div onClick={handleAddRow} className='p-3 rounded-lg bg-blue-200 cursor-pointer border-2 border-black text-center'>
                 Добавить характеристику
             </div>
-            <ProductSpecificationRow />
+            {rows.map(r =>
+                (<ProductSpecificationRow key={r.id} id={r.id} handleDeleteRow={handleDeleteRow} />)
+            )}
         </div>
     )
 }
