a370d45c13707b18e48a41f21220c39b975eb155
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-12 17:49:46 +0300

    Add products count indicator to cart icon

--- online-store/new-client/src/components/navbar/right-side-buttons.tsx
+++ online-store/new-client/src/components/navbar/right-side-buttons.tsx
@@ -1,3 +1,4 @@
+import { useAppSelector } from '@/hooks/hooks';
 import { ArrowLeftOnRectangleIcon, ArrowRightOnRectangleIcon, ShoppingCartIcon, WrenchScrewdriverIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
 import React from 'react'
@@ -9,6 +10,9 @@ const iconStyle = 'w-5 h-5';
 const linkStyle = 'hover:bg-gradient-to-br hover:text-white hover:from-indigo-500 hover:via-sky-600 hover:to-blue-700 block  flex flex-col justify-center items-center mx-1 p-1 rounded-md ';
 
 const RightSideButtons = () => {
+    const cart = useAppSelector(state => state.cart.value)
+    const count = cart.reduce((acc, next) => acc + next.quantity ,0)
+    
     return (
         <div className='flex flex-row absolute right-2 gap-1'>
             <Link href={'/admin'} className={`${linkStyle} cursor-pointer  active:from-purple-950 active:via-red-700 active:to-yellow-600`}>
@@ -31,8 +35,9 @@ const RightSideButtons = () => {
                 </div>
             </Link>
             <Link href='/cart'>
-                <div className={`${linkStyle} cursor-pointer active:from-purple-950 active:via-red-700 active:to-yellow-600`}>
+                <div className={`${linkStyle} relative cursor-pointer active:from-purple-950 active:via-red-700 active:to-yellow-600`}>
                     <ShoppingCartIcon className={iconStyle} />
+                    <div className='absolute bg-lime-200 right-0 top-0 p-1 text-sm text-center w-5 h-5 rounded-full'>{count}</div>
                     <h3 className={textStyle}>Корзина</h3>
                 </div>
             </Link>
