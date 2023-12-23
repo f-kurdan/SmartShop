be99a0fece404cc8641e09f519e99b16956f012f
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-23 12:51:37 +0300

    Add brands and categories resources

--- online-store/server/src/app.module.ts
+++ online-store/server/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common/decorators";
 import { ProductsModule } from "./products/products.module";
 import { ConfigModule } from "@nestjs/config";
+import { CategoriesModule } from './categories/categories.module';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
-    imports: [ProductsModule, ConfigModule.forRoot()]
+    imports: [ProductsModule, ConfigModule.forRoot(), CategoriesModule, BrandsModule]
 })
 export class AppModule {}
