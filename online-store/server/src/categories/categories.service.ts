c232571752fc113b3d69dce0b73b5ba8943d902c
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-23 13:39:01 +0300

    Update category service

--- online-store/server/src/categories/categories.service.ts
+++ online-store/server/src/categories/categories.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from '../prisma/prisma.service';
+import convertToSlug from '../utils/convertToSlug';
+import { CreateCategoryDto } from './dto/createCategoryDto';
 @Injectable()
-export class CategoriesService {}
+export class CategorysService {
+    constructor(private prisma: PrismaService) { }
+
+    getAllCategories() {
+        return this.prisma.category.findMany()
+    }
+
+    getOneCategory(id: string) {
+        return this.prisma.category.findUnique({
+            where: { id: +id }
+        })
+    }
+
+    createCategory(dto: CreateCategoryDto) {
+        return this.prisma.category.create({
+            data: {
+                name: dto.name,
+                slug: convertToSlug(dto.name)
+            }
+        })
+    }
+}
