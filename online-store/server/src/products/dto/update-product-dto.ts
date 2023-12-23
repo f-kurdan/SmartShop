9cc310b621e83551f760e431e623ccd59579c222
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-23 16:28:22 +0300

    Create update product dto

---
+++ online-store/server/src/products/dto/update-product-dto.ts
@@ -0,0 +1,49 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsObject, IsOptional, IsString, MinLength } from "class-validator";
+
+export class UpdateProductDto {
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    name: string;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsNumber()
+    price: number;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsObject()
+    category: {
+        name: string;
+        slug: string;
+    };
+
+    @ApiProperty()
+    @IsOptional()
+    @IsObject()
+    brand: {
+        name: string;
+        slug: string;
+    };
+
+    @ApiProperty()
+    @IsOptional()
+    @IsNumber()
+    quantity: number;
+
+    @ApiProperty()
+    @IsOptional()
+    images: string[];
+
+    @ApiProperty()
+    @IsOptional()
+    @IsObject()
+    productInfo: {
+        name: string;
+        description: string;
+    }
+}
