0cde38a2dcec0951f032b8169275bd647f331dba
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-23 12:44:47 +0300

    Update user service

--- online-store/server/src/users/users.service.ts
+++ online-store/server/src/users/users.service.ts
@@ -1,14 +1,39 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { CreateUserDTO } from "./dto/createUserDto";
 
 @Injectable()
 export class UsersService {
     constructor(private prisma: PrismaService) {}
     getAllUsers() {
-        return this.prisma.user.findMany();
+        return this.prisma.user.findMany({
+            select: {
+                username: true,
+                email: true,
+                role: true,
+                orders: true,
+            } 
+        });
     }
 
     getOneUser(id: number) {
-        return this.prisma.user.findUnique({ where: { id: id} })
+        return this.prisma.user.findUnique({ 
+            where: { id: id},
+            select: {
+                username: true,
+                email: true,
+                role: true,
+                orders: true,
+            } })
+    }
+
+    createUser(dto: CreateUserDTO) {
+        return this.prisma.user.create({
+            data: { 
+                username: dto.name,
+                email: dto.email,
+                password: dto.password,
+            }
+        })
     }
 }
