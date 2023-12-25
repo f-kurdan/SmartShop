d464ebe5c47769af23d38dfbe51f714434952da3
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-25 14:25:35 +0300

    Updte nest configs

--- online-store/new-client/next.config.js
+++ online-store/new-client/next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'images.unsplash.com',
-      port: '',
-      pathname: '/**',
-    },
-  ],}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    domains: ['localhost']
+  },
 }
 
 module.exports = nextConfig
