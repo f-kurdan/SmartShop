8567969763aba54ef8bd913df17ed6c9d36d8ac2
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-27 18:54:58 +0300

    Update product service in the client

--- online-store/new-client/src/services/product.service.ts
+++ online-store/new-client/src/services/product.service.ts
@@ -1,9 +1,15 @@
 import { product } from "../types";
 
 // import { products } from "@/data";
-export async function getProducts(page?: number, query?: string, categoriesId?: number[], brands?: number[],  specifications?: string[]) {
+export async function getProducts(page?: number, searchTerm?: string, categories?: string[], brands?: string[],  specifications?: string[]) {
+    const searchParams = new URLSearchParams();
+    if (page) searchParams.append('page', page.toString());
+    if (searchTerm) searchParams.append('search_term', searchTerm);
+    if (categories) searchParams.append('categories', categories.join('%2C'))
+    if (brands) searchParams.append('brands', brands.join('%2C'))
+    if (specifications) searchParams.append('specs', specifications.join('%2C'))
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_STOREAPI_URL}/products?page=${page}`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_STOREAPI_URL}/products?${searchParams}`)
     const data: { products: product[], totalPages: number } = await res.json()
 
     return { products: data.products, totalPages: data.totalPages }
