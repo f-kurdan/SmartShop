e0c115af4e93502d0496ab882305832afec11a61
Kurdanov Feytullah <fkurdan@gmail.com>
2023-12-31 11:49:00 +0300

    Fix dropdown links

--- online-store/new-client/src/components/navbar/catalog-dropdown.tsx
+++ online-store/new-client/src/components/navbar/catalog-dropdown.tsx
@@ -19,7 +19,7 @@ const CatalogDropdown = () => {
             <div className="shadow-md absolute top-12 w-40 bg-white hidden z-10 group-hover:block">
                 {isCategoriesLoading ? (<span>Идет загрузка...</span>)
                 : categories?.length ? categories?.map((cat) =>
-                    (<Link key={cat.id} className='py-1 hover:bg-gradient-to-br hover:text-white hover:from-indigo-500 hover:via-sky-600 hover:to-blue-700 block active:from-purple-950 active:via-red-700 active:to-yellow-600' href={createURL(cat.id)}>{cat.name.toUpperCase()}</Link>)
+                    (<Link key={cat.id} className='py-1 hover:bg-gradient-to-br hover:text-white hover:from-indigo-500 hover:via-sky-600 hover:to-blue-700 block active:from-purple-950 active:via-red-700 active:to-yellow-600' href={createURL(cat.slug)}>{cat.name.toUpperCase()}</Link>)
                 ) : (
                     <span>Нет данных...</span>
                 )}
