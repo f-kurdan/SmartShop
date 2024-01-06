2000b9aaf004be0db63944d6429a88b11d92cc42
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-06 13:33:37 +0300

    Update search

--- online-store/new-client/src/components/navbar/search.tsx
+++ online-store/new-client/src/components/navbar/search.tsx
@@ -10,6 +10,7 @@ function onlyUnique(value: string, index: number, array: string[]) {
 }
 
 const Search = () => {
+    const [query, setQuery] = useState('')
     const searchParams = useSearchParams()
     const params = new URLSearchParams(searchParams)
     const pathName = usePathname()
@@ -17,25 +18,26 @@ const Search = () => {
 
     const inputRef = useRef<HTMLInputElement>(null)
 
-    const { data } = useProducts()
+    const { data } = useProducts({query: query})
 
     const [searchOptions, setSearchOptions] = useState<string[]>()
 
     const placeholder = 'Введите название товара'
 
-    const handleChange = useDebouncedCallback((value: string) => {
-        if (value) {
-            let products = data?.products.filter(product => product.name.toLowerCase().includes(value.toLowerCase()))
+    const handleChange = useDebouncedCallback((e:React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value)
+        // if (value) {
+        //     let products = data?.products.filter(product => product.name.toLowerCase().includes(value.toLowerCase()))
 
-            let options: string[] | undefined = []
-            options = products?.map(p => p.name)
-                .filter(onlyUnique)
-                .slice(0, 10)
+        //     let options: string[] | undefined = []
+        //     options = products?.map(p => p.name)
+        //         .filter(onlyUnique)
+        //         .slice(0, 10)
 
-            setSearchOptions(!!options?.length ? options : [`По запросу ${value} ичего не найдено`])
-        }
-        else setSearchOptions([])
-    }, 300)
+        //     setSearchOptions(!!options?.length ? options : [`По запросу ${value} ичего не найдено`])
+        // }
+        // else setSearchOptions([])
+    }, 500)
 
     const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
@@ -74,18 +76,16 @@ const Search = () => {
             <input
                 className="peer active:outline bg-gray-100 focus:outline-cyan-200 w-full block rounded-xl py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 placeholder={placeholder}
-                onChange={(e) => {
-                    handleChange(e.target.value);
-                }}
+                onChange={handleChange}
                 ref={inputRef}
                 onKeyDown={handleSearch}
                 defaultValue={searchParams.get('query')?.toString()}
             />
             <MagnifyingGlassIcon onClick={onIconClick} className="absolute cursor-pointer left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             <div className='bg-white w-fit absolute top-full px-3 rounded-md shadow-md hidden group-focus-within:block hover:block'>
-                {!!searchOptions?.length && searchOptions.map((option, index) =>
+                {!!data?.products?.length && data?.products?.map((option, index) =>
                 (
-                    <div key={index} className='cursor-pointer text-sm p-2 my-2 hover:bg-gradient-to-br hover:text-white hover:from-indigo-500 hover:via-sky-600 hover:to-blue-700 block active:from-purple-950 active:via-red-700 active:to-yellow-600 rounded-md ' onClick={() => onOptionClick(option.toLowerCase())}>{option}</div>
+                    <div key={index} className='cursor-pointer text-sm p-2 my-2 hover:bg-gradient-to-br hover:text-white hover:from-indigo-500 hover:via-sky-600 hover:to-blue-700 block active:from-purple-950 active:via-red-700 active:to-yellow-600 rounded-md ' onClick={() => onOptionClick(option.name)}>{option.name}</div>
                 ))}
             </div>
         </div>
