8d5966b00d22d188a74f35dadbcdf2c3e6fad15e
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-09 13:57:57 +0300

    Update admin panel style

--- online-store/new-client/src/components/admin/dialog.tsx
+++ online-store/new-client/src/components/admin/dialog.tsx
@@ -3,7 +3,7 @@ import Form from './form'
 
 const Dialog = ({ name, toShow: state, title }: {name: string, toShow: boolean, title: string }) => {
   return (
-    <dialog open={state} className='fixed top-1/4 transition-all duration-100 z-10 bg-white rounded-lg shadow-lg min-h-[350px] w-1/2' >
+    <dialog open={state} className='fixed top-1/4 transition-all duration-100 z-50 bg-white rounded-lg shadow-lg min-h-[350px] w-1/2' >
       <div className={`flex gap-16 flex-col items-center justify-start p-5`}>
         <div className='font-bold text-3xl text-center text-gray-600'>
           {title}
--- online-store/new-client/src/pages/admin/index.tsx
+++ online-store/new-client/src/pages/admin/index.tsx
@@ -35,25 +35,26 @@ const index = () => {
     }
   }
 
+  const toBloor = showBrandModal || showCategoryModal || showProductAddingModal
   return (
     <HandlerContext.Provider value={handleCancelClick}>
       <div onClick={onClickOutside} className={`${montserrat.className} flex flex-col justify-start items-center px-10 py-14 w-11/12 h-screen my-2 ounded-sm border border-gray-200 bg-gray-50 shadow-lg text-gray-700 gap-y-16`}>
-        <h1 className='font-bold text-5xl text-center text-gray-600 '>Панель администратора</h1>
-        <div onClick={() => setShowCategoryModal(true)} className='transition-all duration-300 bg-purple-300 p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black '>
+        <h1 className={`font-bold text-5xl text-center text-gray-600  ${toBloor? 'blur-md' : '' }`} >Панель администратора</h1>
+        <div onClick={() => setShowCategoryModal(true)} className={`transition-all duration-300 bg-purple-300 p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black  ${toBloor? 'blur-md' : '' }`} >
           Добавить категорию
         </div>
         <Dialog
           name='category'
           toShow={showCategoryModal}
           title='Создание категории' />
-        <div onClick={() => setShowBrandModal(true)} className='transition-all duration-30 bg-lime-300  p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black'>
+        <div onClick={() => setShowBrandModal(true)} className={`transition-all duration-30 bg-lime-300  p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black ${toBloor? 'blur-md' : '' }`}>
           Добавить бренд
         </div>
         <Dialog
           name='brand'
           toShow={showBrandModal}
           title='Создание бренда' />
-        <div onClick={() => setShowProductAddingModal(true)} className='transition-all duration-300 bg-cyan-300  p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black '>
+        <div onClick={() => setShowProductAddingModal(true)} className={`transition-all duration-300 bg-cyan-300  p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black  ${toBloor? 'blur-md' : '' }`}>
           Добавить товар
         </div>
         <NameContext.Provider value='product' >
