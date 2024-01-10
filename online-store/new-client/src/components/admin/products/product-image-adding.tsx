274aba27edd26ce53442273b73e932de0ca2e98a
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-10 15:57:33 +0300

    Update display of selected images

--- online-store/new-client/src/components/admin/products/product-image-adding.tsx
+++ online-store/new-client/src/components/admin/products/product-image-adding.tsx
@@ -1,30 +1,28 @@
 import React, { ChangeEvent, MouseEvent, useRef, useState } from 'react'
 
 const ProductImageAdding = () => {
-  const [images, setImages] = useState([''])
+  const [images, setImages] = useState<string[]>([])
   const imageRef = useRef(null)
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setImages([...images, e.target.value])
-  }
-
-  const onCancelClick = (image: string) => {
-    setImages(images.filter(i => i !== image))
+    if (e.target.files) {
+      const files = Array.from(e.target.files)
+      setImages(files.map(f => f.name));    
+    }
   }
 
   return (
     <>
-    <div ></div>
       <label className='border-2 border-solid rounded-md p-3'>
         Добавить изображения
         <input ref={imageRef} onChange={onInputChange} hidden type="file" multiple name="files[]" />
       </label>
-      {images.length ? images.map(i =>
-      <div className='flex flex-row gap-2 justify-between items-center'>
-        <span key={i} className='text-lime-500 text-center'>{i}</span>
-        <span onClick={() => onCancelClick(i) }>x</span>
-      </div>
-      ) : null}
+      {images?.length ? (<div className='flex flex-col gap-1 justify-start items-start'>
+        {images?.map(i =>
+          <span key={i} className='text-lime-500 text-center'>{i}</span>
+        )}
+      </div>) : null
+      }
     </>
   )
 }
