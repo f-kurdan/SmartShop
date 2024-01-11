0c961dd43df47df8de934d8007621cf7892f41b0
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-11 17:17:54 +0300

    Update product creating dialog

--- online-store/new-client/src/components/admin/products/product-creating-dialog.tsx
+++ online-store/new-client/src/components/admin/products/product-creating-dialog.tsx
@@ -2,20 +2,26 @@ import React, { FormEvent } from 'react'
 import useCategories from '@/hooks/useCategories';
 import useBrands from '@/hooks/useBrands';
 import CancelButton from '../cancel-button'
-import ProductSpecificationAdding from './product-specification-adding';
-import ProductImageAdding from './product-image-adding';
+import ProductSpecificationInputs from './product-specification-inputs';
+import ProductImageInputs from './product-image-inputs';
 import { brand, category, ProductFormInputs, ProductInfo } from '../../../types';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
+import ProductNameAndPriceInputs from './product-name-and-priceInputs';
 
 const ProductCreatingDialog = ({ state, name, title }: { state: boolean, name: string, title: string }) => {
-    const { data: categories } = useCategories();
-    const { data: brands } = useBrands();
-
     const {
         register,
         handleSubmit,
+        control,
         formState: { errors },
-    } = useForm<ProductFormInputs>()
+    } = useForm<ProductFormInputs>({
+        defaultValues: {
+            specs: [{
+                specName: 'Цвет',
+                specDescription: "Белый"
+            }]
+        }
+    })
 
     const onSubmit: SubmitHandler<ProductFormInputs> = (data) => {
         const formData = new FormData();
@@ -27,49 +33,16 @@ const ProductCreatingDialog = ({ state, name, title }: { state: boolean, name: s
                 <div className='font-bold text-3xl text-center text-gray-600'>
                     {title}
                 </div>
-                <div className='grid grid-cols-2 gap-2 w-11/12 '>
-                    <select defaultValue={"1"} className={`${errors.category ? 'outline outline-red-500' : ''} bg-gray-100 rounded-lg h-14 p-3`}
-                    {...register("category", {
-                        required: true,
-                    })}>
-                        <option value='1' disabled hidden>Выберите категорию</option>
-                        {categories?.map(category =>
-                            <option key={category.id} value={category.id} className='my-5'>{category.name}</option>)}
-                    </select>
-                    <select defaultValue={"1"} className={`${errors.brand ? 'outline outline-red-500' : ''} bg-gray-100 rounded-lg h-14 p-3`} 
-                    {...register("brand", {
-                        required: true,
-                    })}>
-                        <option value='1' disabled hidden>Выберите бренд</option>
-                        {brands?.map(brand =>
-                            <option key={brand.id} value={brand.id} >{brand.name}</option>)}
-                    </select>
-                </div>
-                <div className='grid grid-cols-2 gap-2 w-11/12 '>
-                    <input 
-                    type="text" 
-                    placeholder='Введите название' 
-                    className={`bg-gray-100 rounded-lg h-14 p-4 ${errors.name ? 'outline outline-red-500' : ''}`}
-                    {...register("name", {
-                        required: "Введите название",
-                        minLength: {
-                            value: 3,
-                            message: "Название должно содержать минимум 3 символа"
-                        }
-                    })}/>
-                    <input 
-                    type="number"
-                     placeholder='Введите стоимость' 
-                     className={`bg-gray-100 rounded-lg h-14 p-4 ${errors.price ? 'outline outline-red-500' : ''}`} 
-                     {...register("price", {
-                        required: "Укажите цену"
-                    })}/>
-                </div>
-                <ProductImageAdding 
-                register={register}
-                error={errors.images}
+                <ProductNameAndPriceInputs
+                    register={register}
+                    errors={errors} />
+                <ProductImageInputs
+                    register={register}
+                    error={errors.images}
                 />
-                <ProductSpecificationAdding />
+                <ProductSpecificationInputs
+                    register={register}
+                    control={control} />
             </form>
             {/* <SaveButton /> */}
             <CancelButton name={name} />
