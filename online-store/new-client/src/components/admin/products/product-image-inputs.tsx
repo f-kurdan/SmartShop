15a4cda4fb2b7a6b0b0876db9372a172576aadcc
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-14 08:25:33 +0300

    Add product quantity input

--- online-store/new-client/src/components/admin/products/product-image-inputs.tsx
+++ online-store/new-client/src/components/admin/products/product-image-inputs.tsx
@@ -2,28 +2,35 @@ import React, { ChangeEvent, MouseEvent, useRef, useState } from 'react'
 import { FieldError, UseFormRegister } from 'react-hook-form'
 import { ProductFormInputs } from '../../../types'
 
-const ProductImageAdding = ({register, error}:{
-  register: UseFormRegister<ProductFormInputs>, 
-  error?: FieldError}) => {
+const ProductImageAdding = ({ register, error }: {
+  register: UseFormRegister<ProductFormInputs>,
+  error?: FieldError
+}) => {
   const [images, setImages] = useState<string[]>([])
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const files = Array.from(e.target.files)
-      setImages(files.map(f => f.name));    
+      setImages(files.map(f => f.name));
     }
   }
 
   return (
     <div className='grid grid-cols-2 gap-2 w-11/12 justify-start items-start'>
-      <label onChange={onInputChange}  className={`${error? 'outline outline-red-500' : ''} inline border-2 border-solid rounded-md p-3`}>
-        Добавить изображения
-        <input 
-        hidden type="file" 
-        multiple 
-        {...register("name", {
-          required: true
-        })} />
+      <label onChange={onInputChange} className={`${error ? 'outline outline-red-500' : ''} flex flex-row items-center border-2 border-solid rounded-lg p-4 h-14`}>
+        <span>Добавить изображения</span>
+        <input
+          hidden type="file"
+          multiple
+          {...register("name", {
+            required: true
+          })} />
+      </label>
+      <label>
+        <input
+          type="number"
+          placeholder='Количество'
+          className={`bg-gray-100 w-full rounded-lg h-14 p-4`} />
       </label>
       {images?.length ? (<div className='flex flex-col gap-1 justify-start items-start'>
         {images?.map(i =>
