dee409441249741ccfd75e279874c8dff06fc986
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-20 18:31:31 +0300

    Update admin panel

--- online-store/new-client/src/pages/admin/index.tsx
+++ online-store/new-client/src/pages/admin/index.tsx
@@ -9,9 +9,9 @@ const index = () => {
   return (
     <div className={`flex flex-row gap-2 p-2 w-[80%] `}>
       <div id='buttons-list' className={`flex flex-col justify-center gap-2 p-3 w-[35%] bg-gray-50 text-center`}>
-        <h1 onClick={() => setSelectedList('categories')} className='rounded-lg bg-white p-3 cursor-pointer'>Список категорий</h1>
-        <h1 onClick={() => setSelectedList('brands')} className='rounded-lg bg-white p-3 cursor-pointer'>Список брендов</h1>
-        <h1 onClick={() => setSelectedList('products')} className='rounded-lg bg-white p-3 cursor-pointer'>Список товаров</h1>
+        <h1 onClick={() => setSelectedList('categories')} className={`${selectedList === 'categories' ? 'outline-2 outline outline-blue-500' : ''} rounded-lg bg-white p-3 cursor-pointer`}>Список категорий</h1>
+        <h1 onClick={() => setSelectedList('brands')} className={`${selectedList === 'brands' ? 'outline-2 outline outline-blue-500' : ''} rounded-lg bg-white p-3 cursor-pointer`}>Список брендов</h1>
+        <h1 onClick={() => setSelectedList('products')} className={`${selectedList === 'products' ? 'outline-2 outline outline-blue-500' : ''} rounded-lg bg-white p-3 cursor-pointer`}>Список товаров</h1>
       </div>
       {selectedList === 'categories' ?
         (<CategoriesAdminList />) :
