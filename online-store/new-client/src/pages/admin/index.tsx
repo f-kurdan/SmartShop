ab0f7c226df8beca2319503165d335ec2e36eb7b
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-20 16:29:28 +0300

    Update admin panel

--- online-store/new-client/src/pages/admin/index.tsx
+++ online-store/new-client/src/pages/admin/index.tsx
@@ -1,50 +1,50 @@
-import Dialog from '@/components/admin/dialogs/dialog';
-import ProductCreatingDialog from '@/components/admin/dialogs/products/product-creating-dialog';
-import { HandlerContext, NameContext } from '@/contexts/Contexts';
-import { montserrat } from '@/styles/fonts'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import EntitiyCreationSection from '../../components/admin/entitiy-creation-section';
+import CategoriesAdminList from '../../components/admin/lists/categories-list';
+import BrandsAdminList from '../../components/admin/lists/brands-list';
+import ProductsAdminList from '../../components/admin/lists/products-list';
 
 const index = () => {
   const [showCategoryModal, setShowCategoryModal] = useState(false);
   const [showBrandModal, setShowBrandModal] = useState(false);
   const [showProductAddingModal, setShowProductAddingModal] = useState(false);
+  const [toShow, setToShow] = useState(false)
+  const [selectedList, setSelectedList] = useState('');
+  
 
   const onClickOutside = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    // e.preventDefault()
-    if (e.target === e.currentTarget) {
-      if (showCategoryModal)
-        setShowCategoryModal(false)
-      if (showBrandModal)
-        setShowBrandModal(false)
-      if (showProductAddingModal)
-        setShowProductAddingModal(false)
-    }
-  }
 
-  const handleCancelClick = (value: string) => {
-    switch (value) {
-      case 'category':
-        setShowCategoryModal(false);
-        break;
-      case 'brand':
-        setShowBrandModal(false)
-        break
-      case 'product':
-        setShowProductAddingModal(false)
-        break;
-    }
+//     if (showCategoryModal)
+//     setShowCategoryModal(false)
+// if (showBrandModal)
+//     setShowBrandModal(false)
+// if (showProductAddingModal)
+//     setShowProductAddingModal(false)
   }
 
   const toBloor = showBrandModal || showCategoryModal || showProductAddingModal
   return (
-    <div className='flex flex-row gap-2 p-2'>
-      <div className='flex flex-col justify-center gap-2 p-3 w-[35%] bg-gray-50 text-center '>
-        <h1 className='rounded-lg bg-white p-3 cursor-pointer'>Список категорий</h1>
-        <h1 className='rounded-lg bg-white p-3 cursor-pointer'>Список брендов</h1>
-        <h1 className='rounded-lg bg-white p-3 cursor-pointer'>Список товаров</h1>
+    <div onClick={onClickOutside} className={`flex flex-row gap-2 p-2 w-[80%] `}>
+      <div id='buttons-list' className={`flex flex-col justify-center gap-2 p-3 w-[35%] bg-gray-50 text-center ${toBloor ? 'blur-md' : ''}`}>
+        <h1 onClick={() => setSelectedList('categories')} className='rounded-lg bg-white p-3 cursor-pointer'>Список категорий</h1>
+        <h1 onClick={() => setSelectedList('brands')} className='rounded-lg bg-white p-3 cursor-pointer'>Список брендов</h1>
+        <h1 onClick={() => setSelectedList('products')} className='rounded-lg bg-white p-3 cursor-pointer'>Список товаров</h1>
       </div>
-      <EntitiyCreationSection />
+      {selectedList === 'categories'? 
+      (<CategoriesAdminList/>) : 
+      selectedList === 'brands' ?
+    (<BrandsAdminList />) :
+    selectedList === 'products' ?
+    (<ProductsAdminList />) :
+      (<EntitiyCreationSection 
+        showCategoryModal={showCategoryModal}
+        showBrandModal={showBrandModal}
+        showProductAddingModal={showProductAddingModal}
+        setShowCategoryModal={setShowCategoryModal}
+        setShowBrandModal={setShowBrandModal}
+        setShowProductAddingModal={setShowProductAddingModal}  
+        />)
+      }
     </div>
   )
 }
--- online-store/new-client/src/pages/index.tsx
+++ online-store/new-client/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Categories from '@/components/home-page/categories'
 
 export default function Home() {
   return (
-      <div className={`flex flex-col gap-5 justify-around items-center h-5/6 min-h-screen`}>
+      <div className={`flex flex-col gap-5 justify-around items-center min-h-[90vh]`}>
         <Banner />
         <Categories />
       </div>
