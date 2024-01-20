88800a6c91dd903c0d0b3ffc7df7073e10c7d1aa
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-20 16:37:52 +0300

    Update admin products section

--- online-store/new-client/src/components/admin/entitiy-creation-section.tsx
+++ online-store/new-client/src/components/admin/entitiy-creation-section.tsx
@@ -39,13 +39,6 @@ const EntitiyCreationSection = ({
         <HandlerContext.Provider value={handleCancelClick}>
             <div className={`${montserrat.className} flex flex-col justify-start items-center px-10 py-14 full h-[85vh] ounded-sm borde bg-gray-50 shadow-lg text-gray-700 gap-y-16`}>
                 <h1 className={`font-bold text-5xl text-center text-gray-600 ${toBloor ? 'blur-md' : ''} `} >Панель администратора</h1>
-                <div onClick={() => setShowCategoryModal(true)} className={`transition-all duration-300 bg-purple-300 p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black  ${toBloor ? 'blur-md' : ''}`} >
-                    Добавить категорию
-                </div>
-                <Dialog
-                    name='category'
-                    toShow={showCategoryModal}
-                    title='Создание категории' />
                 <div onClick={() => setShowBrandModal(true)} className={`transition-all duration-30 bg-lime-300  p-4 text-center w-1/2 rounded-xl cursor-pointer active:blur-sm border-2 border-black ${toBloor ? 'blur-md' : ''}`}>
                     Добавить бренд
                 </div>
