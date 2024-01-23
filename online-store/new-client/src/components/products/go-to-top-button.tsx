0ea8a0484b034e9482cc762d79df25f958c9af72
Kurdanov Feytullah <fkurdan@gmail.com>
2024-01-23 15:57:25 +0300

    Update catalog layout

--- online-store/new-client/src/components/products/go-to-top-button.tsx
+++ online-store/new-client/src/components/products/go-to-top-button.tsx
@@ -20,7 +20,7 @@ const GoToTopButton = () => {
   }, [])
 
   return (
-    <div id='goToTop' className={`transition-all duration-300 bg-black border-2 rounded-full fixed bottom-10 right-16 p-2 hover:outline hover:outline-8 hover:outline-cyan-200 ${hide ? "opacity-0" : "opacity-100"}`}>
+    <div id='goToTop' className={`transition-all duration-300 bg-black border-2 rounded-full fixed bottom-10 right-16 p-2 hover:outline hover:outline-8 hover:outline-cyan-200`}>
       <Link href='#' >
         <ArrowUpIcon width={40} height={40} color='white' />
       </Link>
--- online-store/new-client/src/pages/catalog/index.tsx
+++ online-store/new-client/src/pages/catalog/index.tsx
@@ -5,22 +5,17 @@ import { useSearchParams } from 'next/navigation';
 const Page = () => {
   const searchParams = useSearchParams()
   const params = new URLSearchParams(searchParams)
-  const query = params.get('query')?.toString()  
-  const page = params.get('page') 
+  const query = params.get('query')?.toString()
+  const page = params.get('page')
   const categories = params.get('category')?.split(';')
   const brands = params.get('brand')?.split(';')
   const specifications = params.get('specifications')?.split(';')
-  const {data } = useProducts({page: Number(page), query, categories, brands, specifications});  
+  const { data } = useProducts({ page: Number(page), query, categories, brands, specifications });
 
   return (
-    <>
-    <Catalog products={data?.products!} 
-    totalPages={data?.totalPages!} 
+    <Catalog products={data?.products!}
+      totalPages={data?.totalPages!}
     />
-      <div className='h-[100vh] border-2 border-black'></div>
-
-
-    </>
   )
 }
 
