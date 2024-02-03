a86f3c7576488ff48fd25f9d3b49566b7dba192a
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-03 10:44:48 +0300

    Update categories server service

--- online-store/server/src/categories/categories.service.ts
+++ online-store/server/src/categories/categories.service.ts
@@ -11,7 +11,7 @@ export class CategoriesService {
 
     async getAllCategories() {
         return await this.prisma.category.findMany({
-            orderBy: { name: 'desc' }
+            orderBy: { name: 'asc' }
         })
     }
 
