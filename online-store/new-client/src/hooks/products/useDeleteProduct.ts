4a8805a075536876f0c9ae369845fd8e19cc3b71
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-03 10:42:15 +0300

    Add hook for deleting products

---
+++ online-store/new-client/src/hooks/products/useDeleteProduct.ts
@@ -0,0 +1,15 @@
+import { useMutation, useQueryClient } from 'react-query'
+import productService from '../../services/product.service'
+
+const useDeleteProduct = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: productService.deleteProduct,
+        onSuccess: async () => {
+            await queryClient.invalidateQueries(['products'])
+        }
+    })
+}
+
+export default useDeleteProduct
