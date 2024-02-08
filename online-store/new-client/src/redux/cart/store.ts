e1a23e588c72695d8109eaf753efc5ad50b90ea0
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-08 15:34:34 +0300

    Fix redux-persist middleware error

--- online-store/new-client/src/redux/cart/store.ts
+++ online-store/new-client/src/redux/cart/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './cartSlice'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const persistConfig = {
     key: 'root',
-    storage,
+    storage: AsyncStorage,
 }
 
 const persistedCartReducer = persistReducer(persistConfig, cartReducer)
@@ -14,8 +14,9 @@ export const store = configureStore({
     reducer: {
         "cart": persistedCartReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-export const persistor = persistStore(store)
+export const persistor = persistStore(store) 
