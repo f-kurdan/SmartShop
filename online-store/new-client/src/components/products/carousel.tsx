20ffc4505d975eb6777ab0e73e1024bc61929d09
Kurdanov Feytullah <fkurdan@gmail.com>
2024-02-21 14:36:13 +0300

    Update images carousel

--- online-store/new-client/src/components/products/carousel.tsx
+++ online-store/new-client/src/components/products/carousel.tsx
@@ -2,7 +2,7 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image'
 import React, { useState } from 'react'
 
-const Carousel = ({images}:{images:string[]}) => {
+const Carousel = ({ images }: { images: string[] }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const onLeftClick = () => {
@@ -18,13 +18,11 @@ const Carousel = ({images}:{images:string[]}) => {
     }
 
     return (
-        <div className='transition-all duration-300 group relative w-fit max-h-fit '>
+        <div className='flex flex-row justify-center transition-all duration-300 group relative w-fit max-h-fit border-[0.5px] border-gray-300 rounded-2xl min-w-[50%] min-h-[400px]'>
             <div onClick={onLeftClick} className='transition-all duration-300 group-hover:block hidden cursor-pointer p-4 absolute left-1 top-1/2 rounded-full bg-cyan-100 opacity-70' >
                 <ArrowLeftIcon width={20} height={20} />
             </div>
-            <div className='max-w-[300px] overflow-hidden'>
-            <Image className='rounded-2xl object-contain' src={`${process.env.NEXT_PUBLIC_STOREAPI_URL}/${images[currentIndex]}`} alt={'data!.name'} width={500} height={500} />
-            </div>
+            <Image className=' rounded-2xl object-contain overflow-hidden max-h-[400px] ' src={`${process.env.NEXT_PUBLIC_STOREAPI_URL}/${images[currentIndex]}`} alt={'data!.name'} width={400} height={250} />
             <div onClick={onRightClick} className='group-hover:block hidden cursor-pointer p-4 absolute right-1 top-1/2 rounded-full bg-cyan-100 opacity-70' >
                 <ArrowRightIcon width={20} height={20} />
             </div>
