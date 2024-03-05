abf281b23c260005bfa4952a06d42f4dbd27c653
Kurdanov Feytullah <fkurdan@gmail.com>
2024-03-05 16:27:27 +0300

    Create auth module

---
+++ online-store/server/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { PrismaModule } from "nestjs-prisma";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { UsersModule } from "../users/users.module";
+import { JwtStrategy } from "./strategy/jwt.strategy";
+
+export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
+
+@Module({
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
+    imports: [UsersModule,
+        PrismaModule, 
+        PassportModule,
+        JwtModule.register({
+          secret: jwtSecret,
+          signOptions: { expiresIn: '5m' }, 
+        }),],
+})
+export default class AuthModule { }
---
+++ online-store/server/src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { AuthEntity } from "./entity/auth.entity";
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from "../prisma/prisma.service";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService
+    ) {}
+    async login(email: string, password: string): Promise<AuthEntity> {
+        const user = await this.prisma.user.findUnique({ where: { email: email } });
+    
+        if (!user) {
+          throw new NotFoundException(`No user found for email: ${email}`);
+        }
+        
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+        if (!isPasswordValid) {
+          throw new UnauthorizedException('Invalid password');
+        }
+    
+        return {
+          accessToken: this.jwtService.sign({ userId: user.id, sub: user.username }),
+        };
+      }
+}
